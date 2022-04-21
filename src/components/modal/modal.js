fa707b2d9b46338d8ad38d93db4303bbea593007
hamid-murambiwa <machipisajunior@gmail.com>
2022-04-21 11:56:01 +0200

    Update modal JSX and styling

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-// import "./Modal.css";
+import img from '../../style/images/button.png'
+import "../../style/modal.css";
 
 export default function Modal(props) {
   const [modal, setModal] = useState(false);
@@ -17,7 +18,7 @@ export default function Modal(props) {
   return (
     <>
       <button onClick={toggleModal} className="btn-modal">
-        Open
+        View Project
       </button>
 
       {modal && (
@@ -25,12 +26,30 @@ export default function Modal(props) {
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
             <h2>{props.project.name}</h2>
+            <section className="main-modal-content">
+            <div className="modal-img-con">
+              <img src={props.project.image} alt={props.project.name} className="modal-img" />
+            </div>
+            <div className="modal-text-con">
             <p>{props.project.description}</p>
+            <div className="modal-tags">
             {props.project.tags.map(tag => (
-                <p>{tag.name}</p>
+                <p>{tag}</p>
             ))}
+            </div>
+            </div>
+            </section>
+            <div className="modal-link-btn-con">
+            <a href={props.project.live}>
+              View Live
+            </a>
+            {console.log(props.project.link)}
+            <a href={props.project.github}>
+              View Source
+            </a>
+            </div>
             <button className="close-modal" onClick={toggleModal}>
-              CLOSE
+              <img src={img} alt="exit button icon" className="modal-exit-btn" />
             </button>
           </div>
         </div>
