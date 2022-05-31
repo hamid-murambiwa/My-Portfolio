2e874947ee2b3ad4214b3f359d504731b3bc039a
hamid-murambiwa <machipisajunior@gmail.com>
2022-05-31 08:44:05 +0200

    Bug fix

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -4,7 +4,7 @@ import '../../style/projects.css';
 const projects = [
   {
     name: 'Recipe App',
-    description: 'The Recipe app keeps track of all your recipes, ingredients, and inventory. It will allow you to save ingredients, keep track of what you have, create recipes, and generate a shopping list based on what you have and what you are missing from a recipe. Also, since sharing recipes is an important part of cooking the app should allow you to make them public so anyone can access them.',
+    description: 'The recipe app allows users to view various recipes & prepare good tasty meals.',
     short_d: 'The Recipe app keeps track of all your recipes, ingredients, and inventory.',
     live: 'https://awe-recipe-app.herokuapp.com/',
     github: 'https://github.com/hamid-murambiwa/Recipe-app',
